fdccebb4531f7afea1f19a40e26d441c8a73c70d
afedorovw <83913084+afedorovw@users.noreply.github.com>
2021-08-16 23:59:40 +0300

    Add project

---
+++ src/js/script.js
@@ -0,0 +1,112 @@
+/*"use strict"*/
+
+$(document).ready(function() {
+
+    new WOW().init();
+
+    $("#inputTel").mask("+7 (999) 999-9999");
+
+    $('form').submit(function(event){
+
+    if ($("#inputTel").val() == "" || $("#inputEmail3").val() == "" ) {
+        alert("Заполните поля");
+    };
+
+    event.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "php/test.php",
+        data: $(this).serialize()
+    }).done(function(){
+        $(this).find("input").val("");
+        alert("Успешно отправлена!");
+        $("form").trigger("reset");
+    });
+        return false;
+
+    });
+
+    $('#visit').click(function() {
+        alert("Сайт в разработке");
+        });
+
+    $('.inputContact').click(function() {
+        $(this).delay('fast').hide('slow').delay(1000).show(5000);
+        });
+
+    let options = {threshold: [0.5]};
+    let observer = new IntersectionObserver (onEntry, options);
+    let elements = $('.about_text');
+    elements.each((i,el) => {
+        observer.observe(el);
+    });
+});
+
+
+function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        };
+    });
+};
+
+$('a[href^="#"]').click(function(){
+        let valHref = $(this).attr("href");
+        $('html, body').animate({scrollTop: $(valHref).offset().top - 70 + "px"});
+});
+
+
+var number = document.querySelector('.number'),
+        numberTop = number.getBoundingClientRect().top,
+        start = +number.innerHTML, end = +number.dataset.max;
+
+        window.addEventListener('scroll', function onScroll() {
+            if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+                this.removeEventListener('scroll', onScroll);
+                var interval = setInterval(function() {
+                    number.innerHTML = ++start;
+                    if(start == end) {
+                        clearInterval(interval);
+                    };
+                }, 5);
+            }
+        });
+
+
+
+
+function raschitat() {
+type_site  = document.getElementById('type_site').value;
+switch (type_site) {
+   case "vis":
+      cena = 1;
+      break
+   case "promp":
+      cena = 2}
+
+    design  = document.getElementById('design').value;
+switch (design) {
+   case "maket":
+      cena = 1;
+      break
+   case "ntmaket":
+      cena = 2}
+
+    adaptive  = document.getElementById('adaptive').value;
+switch (adaptive) {
+   case "pk":
+      cena = 1;
+      break
+   case "pkm":
+      cena = 2}
+
+design  = document.getElementById('design').value;
+adaptive  = document.getElementById('adaptive').value;
+
+sroki = parseFloat (design) + parseFloat (adaptive);
+document.getElementById('sroki').innerHTML = "Сроки: "+ sroki +" дн";
+stoimost = sroki + type_site;
+document.getElementById('stoimost').innerHTML = "Стоимость: "+ stoimost +" р.";
+};
---
+++ src/php/test.php
@@ -0,0 +1,8 @@
+<?php
+
+$email = $_POST['userEmail'];
+$phone = $_POST['userPhone'];
+
+echo "Скрипт ок <br>". $email . " <br>" . $phone;
+
+>
